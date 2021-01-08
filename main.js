69589e48687cfd4aa9c9198238046687b0ca45f3
swapnanil-cloud <74404855+swapnanil-cloud@users.noreply.github.com>
2021-01-08 13:07:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+var first_name ="Swapnanil";
+console.log(first_name);
+
+var last_name ="das";
+console.log("last_name")
+
+var full_name = first_name.concat(last_name)
+console.log(full_name)
+
+var length_of_full_name = full_name.length;
+console.log(length_of_full_name);
+
+var square_root = Math.sqrt(16); 
+console.log(square_root);
+
+var maximum_number = Math.max(15, 10);
+console.log(maximum_number);
+
+var minimum_number = Math.min(15, 10);
+console.log(minimum_number);
+
+var power_eg_1 = Math.pow(4, 2);
+console.log(power_eg_1);
+
+var power_eg_2 = Math.pow(4, 3);
+console.log(power_eg_2);
